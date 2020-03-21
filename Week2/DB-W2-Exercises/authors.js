86b4510daacec3de256d383a828da6025f08df2a
Muhammad Hussein <m.a.hussein784@gmail.com>
2020-03-22 00:44:59 +0100

    Homework week2

---
+++ Week2/DB-W2-Exercises/authors.js
@@ -0,0 +1,154 @@
+const authors =  [
+	{
+		author_no: 1,
+		author_name: 'Mark',
+		university: 'Amsterdam',
+		date_of_birth: '1965-02-10',
+		h_index: 1100,
+		gender: 'm',
+		friend: 14
+	},
+
+	{
+		author_no: 2,
+		author_name: 'Steve',
+		university: 'Tilburg',
+		date_of_birth: '1976-04-26',
+		h_index: 1150,
+		gender: 'm',
+		friend: 13
+	},
+
+	{
+		author_no: 3,
+		author_name: 'Bel',
+		university: 'Delft',
+		date_of_birth: '1979-07-28',
+		h_index: 1200,
+		gender: 'm',
+		friend: 12
+	},
+
+	{
+		author_no: 4,
+		author_name: 'Margriet',
+		university: 'Groningen',
+		date_of_birth: '1982-08-16',
+		h_index: 1250,
+		gender: 'f',
+		friend: 11
+	},
+
+	{
+		author_no: 5,
+		author_name: 'Lara',
+		university: 'Utrecht',
+		date_of_birth: '1987-05-05',
+		h_index: 1300,
+		gender: 'f',
+		friend: 10
+	},
+
+	{
+		author_no: 6,
+		author_name: 'johan',
+		university: 'Leiden',
+		date_of_birth: '1981-09-10',
+		h_index: 1350,
+		gender: 'm',
+		friend: 9
+	},
+
+	{
+		author_no: 7,
+		author_name: 'Ahmad',
+		university: 'Aleppo',
+		date_of_birth: '1991-05-05',
+		h_index: 1400,
+		gender: 'm',
+		friend: 8
+	},
+
+	{
+		author_no: 8,
+		author_name: 'Moses',
+		university: 'Maastricht',
+		date_of_birth: '1970-02-03',
+		h_index: 1450,
+		gender: 'm',
+		friend: 7
+	},
+
+	{
+		author_no: 9,
+		author_name: 'Rony',
+		university: 'Utrecht',
+		date_of_birth: '1992-04-08',
+		h_index: 1500,
+		gender: 'm',
+		friend: 5
+	},
+
+	{
+		author_no: 10,
+		author_name: 'Samer',
+		university: 'Damascus',
+		date_of_birth: '1984-05-06',
+		h_index: 1550,
+		gender: 'F',
+		friend: 6
+	},
+
+	{
+		author_no: 11,
+		author_name: 'Mustapha',
+		university: 'Eindhoven',
+		date_of_birth: '1988-05-30',
+		h_index: 1600,
+		gender: 'm',
+		friend: 4
+	},
+
+	{
+		author_no: 12,
+		author_name: 'Hakeem',
+		university: 'Amsterdam',
+		date_of_birth: '1978-05-15',
+		h_index: 1650,
+		gender: 'm',
+		friend: 3
+	},
+
+	{
+		author_no: 13,
+		author_name: 'Henrie',
+		university: 'Utrecht',
+		date_of_birth: '1980-01-20',
+		h_index: 1700,
+		gender: 'm',
+		friend: 2
+	},
+
+	{
+		author_no: 14,
+		author_name: 'Jacob',
+		university: 'Leiden',
+		date_of_birth: '1985-05-14',
+		h_index: 1750,
+		gender: 'm',
+		friend: 1
+	},
+
+	{
+		author_no: 15,
+		author_name: 'Julia',
+		university: 'Maastrecht',
+		date_of_birth: '1995-08-26',
+		h_index: 1800,
+		gender: 'f',
+		friend: 10
+	}
+];
+    
+
+module.exports = authors;
