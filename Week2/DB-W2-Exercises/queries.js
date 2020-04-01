f4aed0b4a32dfd4b4329b8a6fd8ac78d32480d8f
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2020-04-02 01:43:26 +0200

    Update W2 Hmework

--- Week2/DB-W2-Exercises/queries.js
+++ Week2/DB-W2-Exercises/queries.js
@@ -10,16 +10,37 @@ const connection = mysql.createConnection({
 const execQuery = util.promisify(connection.query.bind(connection));
 
 async function seedData() {
-	const query1 = `SELECT COUNT(paper_id), COUNT(DISTINCT author_id) FROM research_papers`;
+   const query1 = `SELECT rp.paper_title, COUNT(DISTINCT a.author_no) AS 'Authors'
+   FROM  Authorship a RIGHT JOIN research_papers rp
+   ON rp.paper_id = a.paper_id
+   GROUP BY rp.paper_id`;
 
-	const query2 = `SELECT COUNT(paper_title) FROM research_papers AS rp , 
-                   Authors AS a WHERE a.author_no = rp.author_id AND a.gender ='f' `;
-	const query3 = `SELECT AVG(h_index),university FROM Authors GROUP BY university `;
+	const query2 = `SELECT
+	COUNT(Research_papers.paper_id) AS 'Sum of the research papers published by all female authors'
+	FROM Authorship JOIN Research_papers 
+	ON Authorship.paper_id = Research_papers.paper_id RIGHT JOIN  Authors   
+	ON Authors.author_no = Authorship.author_no 
+	WHERE Authors.gender='f'`;
 
-	const query4 = `SELECT AVG(h_index), university FROM authors AS a inner join research_papers 
-                    AS rp ON a.author_no= rp.author_id group by a.university`;
+	const query3 = `SELECT 
+	AVG(h_index),
+	university 
+	FROM Authors GROUP BY university `;
 
-	const query5 = `SELECT MIN(h_index), MAX(h_index), university FROM Authors GROUP BY university`;
+	const query4 = `SELECT 
+	COUNT(DISTINCT ap.paper_id) AS 'Sum of the research papers',
+	a.university 
+	from authors a 
+	join Authorship ap 
+	on a.author_no = ap.author_no 
+	group by a.university`;
+
+	const query5 = `SELECT
+	MIN(h_index),
+	MAX(h_index),
+	university
+	FROM Authors GROUP BY university`;
+
 	connection.connect();
 	try {
 		console.log(await execQuery(query1));
