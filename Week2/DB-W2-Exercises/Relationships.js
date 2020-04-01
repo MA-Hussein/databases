0e901c84b4510c257f469aa155fb096ff7660f58
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2020-04-02 01:41:42 +0200

    Update W2 Hmework

--- Week2/DB-W2-Exercises/Relationships.js
+++ Week2/DB-W2-Exercises/Relationships.js
@@ -2,6 +2,7 @@ const util = require('util');
 const mysql = require('mysql');
 const authors = require('./authors');
 const papers = require('./papers');
+const Authorship = require('./authorship');
 const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'hyfuser',
@@ -12,26 +13,31 @@ const connection = mysql.createConnection({
 const execQuery = util.promisify(connection.query.bind(connection));
 
 async function seedData() {
-	const createResPTable = `
+	const createResearch_PapersTable = `
     CREATE TABLE IF NOT EXISTS Research_Papers
     ( paper_id INT NOT NULL PRIMARY KEY, paper_title VARCHAR(50),
-    conference VARCHAR(50), publish_date DATE , author_id INT )`;
+    conference VARCHAR(50), publish_date DATE )`;
 
-	const addForeign = `
-    ALTER TABLE Research_Papers ADD CONSTRAINT 
-    fk_Authors_Res_Papers FOREIGN KEY (author_id) 
-    REFERENCES Authors (author_no) `;
+	const createAuthorshipTable = `
+	CREATE TABLE IF NOT EXISTS Authorship
+	( author_no INT,paper_id INT,
+		CONSTRAINT FK_Author FOREIGN KEY(author_no) REFERENCES Authors(author_no),
+		CONSTRAINT FK_Paper FOREIGN KEY(paper_id) REFERENCES Research_Papers(paper_id))`;
 
 	connection.connect();
 
 	try {
-		await execQuery(createResPTable);
-		await execQuery(addForeign);
+		await execQuery(createResearch_PapersTable);
+		
 		authors.forEach(async (author) => {
 			await execQuery('INSERT INTO Authors SET ?', author);
 		});
 		papers.forEach(async (paper) => {
 			await execQuery('INSERT INTO Research_Papers SET ?', paper);
+		});
+		await execQuery(createAuthorshipTable);
+		Authorship.forEach(async (Author_Paper) => {
+			await execQuery('INSERT INTO Authorship SET ?', Author_Paper);
 		});
 		console.log('Executed successfully ..');
 	} catch (error) {
