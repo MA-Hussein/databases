6f6228077ec2cd1942a1373114c28048c2b0e6dd
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2020-04-02 01:49:14 +0200

    Update W2 Hmework

--- Week2/DB-W2-Exercises/papers.js
+++ Week2/DB-W2-Exercises/papers.js
@@ -3,211 +3,181 @@ const papers =  [
 		paper_id: 100,
 		conference: 'conference_1',
 		paper_title: 'title_1',
-		publish_date: '2019-11-10',
-		author_id: 1
+		publish_date: '2019-11-10'
 	},
 	{
 		paper_id: 101,
 		conference: 'conference_2',
 		paper_title: 'title_2',
-		publish_date: '2017-11-10',
-		author_id: 2
+		publish_date: '2017-11-10'
 	},
 	{
 		paper_id: 102,
 		conference: 'conference_3',
 		paper_title: 'title_3',
-		publish_date: '2018-11-10',
-		author_id: 3
+		publish_date: '2018-11-10'
 	},
 	{
 		paper_id: 103,
 		conference: 'conference_4',
 		paper_title: 'title_4',
-		publish_date: '2020-01-10',
-		author_id: 4
+		publish_date: '2020-01-10'
 	},
 	{
 		paper_id: 104,
 		conference: 'conference_5',
 		paper_title: 'title_5',
-		publish_date: '1998-07-07',
-		author_id: 5
+		publish_date: '1998-07-07'
 	},
 	{
 		paper_id: 105,
 		conference: 'conference_6',
 		paper_title: 'title_6',
-		publish_date: '2019-05-05',
-		author_id: 6
+		publish_date: '2019-05-05'
 	},
 	{
 		paper_id: 106,
 		conference: 'conference_7',
 		paper_title: 'title_7',
-		publish_date: '1999-11-10',
-		author_id: 7
+		publish_date: '1999-11-10'
 	},
 	{
 		paper_id: 107,
 		conference: 'conference_8',
 		paper_title: 'title_8',
-		publish_date: '1997-10-10',
-		author_id: 8
+		publish_date: '1997-10-10'
 	},
 	{
 		paper_id: 108,
 		conference: 'conference_9',
 		paper_title: 'title_9',
-		publish_date: '2016-11-10',
-		author_id: 9
+		publish_date: '2016-11-10'
 	},
 	{
 		paper_id: 109,
 		conference: 'conference_10',
 		paper_title: 'title_10',
-		publish_date: '2019-06-10',
-		author_id: 10
+		publish_date: '2019-06-10'
 	},
 	{
 		paper_id: 110,
 		conference: 'conference_11',
 		paper_title: 'title_11',
-		publish_date: '2019-11-11',
-		author_id: 11
+		publish_date: '2019-11-11'
 	},
 	{
 		paper_id: 111,
 		conference: 'conference_12',
 		paper_title: 'title_12',
-		publish_date: '2019-12-12',
-		author_id: 12
+		publish_date: '2019-12-12'
 	},
 	{
 		paper_id: 112,
 		conference: 'conference_13',
 		paper_title: 'title_13',
-		publish_date: '1980-11-10',
-		author_id: 13
+		publish_date: '1980-11-10'
 	},
 	{
 		paper_id: 113,
 		conference: 'conference_14',
 		paper_title: 'title_14',
-		publish_date: '2019-07-10',
-		author_id: 14
+		publish_date: '2019-07-10'
 	},
 	{
 		paper_id: 114,
 		conference: 'conference_15',
 		paper_title: 'title_15',
-		publish_date: '2019-11-01',
-		author_id: 15
+		publish_date: '2019-11-01'
 	},
 	{
 		paper_id: 115,
 		conference: 'conference_16',
 		paper_title: 'title_16',
-		publish_date: '2019-09-09',
-		author_id: 1
+		publish_date: '2019-09-09'
 	},
 	{
 		paper_id: 116,
 		conference: 'conference_17',
 		paper_title: 'title_17',
-		publish_date: '2019-03-10',
-		author_id: 1
+		publish_date: '2019-03-10'
 	},
 	{
 		paper_id: 117,
 		conference: 'conference_18',
 		paper_title: 'title_18',
-		publish_date: '1988-11-10',
-		author_id: 2
+		publish_date: '1988-11-10'
 	},
 	{
 		paper_id: 118,
 		conference: 'conference_19',
 		paper_title: 'title_19',
-		publish_date: '1990-05-10',
-		author_id: 3
+		publish_date: '1990-05-10'
 	},
 	{
 		paper_id: 119,
 		conference: 'conference_20',
 		paper_title: 'title_20',
-		publish_date: '1987-11-10',
-		author_id: 5
+		publish_date: '1987-11-10'
 	},
 	{
 		paper_id: 120,
 		conference: 'conference_21',
 		paper_title: 'title_21',
-		publish_date: '2019-04-10',
-		author_id: 7
+		publish_date: '2019-04-10'
 	},
 	{
 		paper_id: 121,
 		conference: 'conference_22',
 		paper_title: 'title_22',
-		publish_date: '2001-11-10',
-		author_id: 9
+		publish_date: '2001-11-10'
 	},
 	{
 		paper_id: 122,
 		conference: 'conference_23',
 		paper_title: 'title_23',
-		publish_date: '2004-05-10',
-		author_id: 8
+		publish_date: '2004-05-10'
 	},
 	{
 		paper_id: 123,
 		conference: 'conference_24',
 		paper_title: 'title_24',
-		publish_date: '2019-11-10',
-		author_id: 5
+		publish_date: '2019-11-10'
 	},
 	{
 		paper_id: 124,
 		conference: 'conference_25',
 		paper_title: 'title_25',
-		publish_date: '2005-05-10',
-		author_id: 10
+		publish_date: '2005-05-10'
 	},
 	{
 		paper_id: 125,
 		conference: 'conference_26',
 		paper_title: 'title_26',
-		publish_date: '2006-09-10',
-		author_id: 11
+		publish_date: '2006-09-10'
 	},
 	{
 		paper_id: 126,
 		conference: 'conference_27',
 		paper_title: 'title_27',
-		publish_date: '2007-11-10',
-		author_id: 12
+		publish_date: '2007-11-10'
 	},
 	{
 		paper_id: 127,
 		conference: 'conference_28',
 		paper_title: 'title_28',
-		publish_date: '2009-05-10',
-		author_id: 15
+		publish_date: '2009-05-10'
 	},
 	{
 		paper_id: 128,
 		conference: 'conference_29',
 		paper_title: 'title_29',
-		publish_date: '2010-11-10',
-		author_id: 14
+		publish_date: '2010-11-10'
 	},
 	{
 		paper_id: 129,
 		conference: 'conference_30',
 		paper_title: 'title_30',
-		publish_date: '2011-11-11',
-		author_id: 1
+		publish_date: '2011-11-11'
 	}
 ];
 module.exports = papers;
