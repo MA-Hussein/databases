88aa77c3b899a72e5e337ed8ea37e647c08b5caa
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2020-04-06 00:26:05 +0200

    Update tr-insert-values.js

--- Week3/DB-W3-Homework/exercise2/tr-insert-values.js
+++ Week3/DB-W3-Homework/exercise2/tr-insert-values.js
@@ -10,17 +10,11 @@ const connection = mySql.createConnection({
 connection.connect();
 
 const amount = ['insert into accounts value(101, 2000)', 'insert into accounts value(102, 3000)'];
-
-connection.query(amount[0], (err, result) => {
+for (let i = 0; i < amount.length; i++) {   
+connection.query(amount[i], (err, result) => {
     if (err) throw err;
     console.log('The amount has been deposited', result);
-
 });
-
-connection.query(amount[1], (err, result) => {
-    if (err) throw err;
-    console.log('The amount has been deposited', result);
-
-});
+}
 
 connection.end();
